54abe57b81150d21f49f0088456624f75462df7e
ryzenRB <86495244+ryzenRB@users.noreply.github.com>
2021-06-29 12:43:06 +0530

    Add files via upload

---
+++ Print sum, product etc using Java method/src/com/javamethodfor/sum/Main.java
@@ -0,0 +1,22 @@
+package com.javamethodfor.sum;
+
+public class Main {
+
+    // Add these below line in your main.java. if you want to find sum or product.
+    // I am taking sum and product as an example. You can do anything else too using this same method.
+
+    static int sum(int a, int b){
+        return a+b;}
+
+    static int product(int c, int d){
+        return c*d;}
+
+    public static void main(String[] args) {
+	// Now it's time to print out the sum and product. Try to understand carefully how it's done.
+        System.out.println(sum(35,15)); // This will print 35+15
+        System.out.println(product(2, 25)); // This will print 2 x 25.
+
+        // That's all, now time to print and see.
+
+    }
+}
